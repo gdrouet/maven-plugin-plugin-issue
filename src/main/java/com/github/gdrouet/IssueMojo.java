e55ba740fd30ee33db4c481323fda0bbdb80bd59
gdrouet <guidrouet@gmail.com>
2015-04-19 15:43:43 +0200

    Adds the project to reproduce StringIndexOutOfBoundsException

---
+++ src/main/java/com/github/gdrouet/IssueMojo.java
@@ -0,0 +1,19 @@
+package com.github.gdrouet;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+/**
+ * <p></p>
+ */
+@Mojo(name = "process")
+public class IssueMojo extends AbstractMojo {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void execute() throws MojoExecutionException {
+    }
+}
